b793bfa3515f1778cb0f3080699b9e92f81d3be6
A10A <yassine@adlive.io>
2022-03-31 16:48:11 +0200

    Edition site

--- backend/src/config.js
+++ backend/src/config.js
@@ -63,7 +63,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-05-01T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-03-28T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xD2e72912d8a7D151146E6b7B57332f643e687b92"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
